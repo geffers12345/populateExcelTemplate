0ce0163a2ab8d086dba5ce3622249bed16409c34
geffers12345 <31837068+geffers12345@users.noreply.github.com>
2018-07-13 18:32:36 +0800

    first commit

---
+++ PopulatingExcel/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PopulatingExcel.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button runat="server" ID="Populate" OnClick="Populate_Click" Text="Populate" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ PopulatingExcel/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using OfficeOpenXml.Table;
+using OfficeOpenXml;
+using System.IO;
+
+namespace PopulatingExcel
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Populate_Click(object sender, EventArgs e)
+        {
+            string path = Server.MapPath(".") + "/Excel/template.xlsx";
+
+            FileInfo newFile = new FileInfo(path.ToString());
+
+            ExcelPackage pck = new ExcelPackage(newFile);
+            var ws = pck.Workbook.Worksheets["Sheet1"];
+
+            ws.Cells["O4"].Value = "A";
+            ws.Cells["D12"].Value = "Some Name";
+            ws.Cells["N12"].Value = "43534";
+            ws.Cells["D14"].Value = "Last modified";
+            ws.Cells["D16"].Style.Font.Bold = true;
+
+            pck.Save();
+
+            Response.ContentType = "application/excel";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=Sample.xlsx");
+            Response.TransmitFile(Server.MapPath("/Excel/template.xlsx"));
+            Response.End();
+        }
+    }
+}
